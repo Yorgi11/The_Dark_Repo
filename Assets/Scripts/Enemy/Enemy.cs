f31bd3364993abe7c9c1e7818eaca93313139724
Sebastian <sebander01@gmail.com>
2023-03-07 02:20:35 -0500

    Trying to create a nav mesh

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float maxrange;
     [SerializeField] private float mindist;
+
+    private NavMeshAgent navigation;
 
     private float disttotarg;
 
@@ -17,22 +20,56 @@ public class Enemy : MonoBehaviour
 
     private Rigidbody rb;
 
+    private bool seenPlayer = false;
+
+    private RaycastHit rayHit;
+    private Vector3 ray;
+
+    public float sightLine;
+
+    public Player player;
+
     private void Awake()
     {
+        player = FindObjectOfType<Player>();
         rb = GetComponent<Rigidbody>();
         currentHp = maxHp;
         targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
     }
     private void Update()
     {
-        if (rb.velocity.magnitude > speed * 1.5f) rb.velocity = rb.velocity.normalized * speed * 1.5f;
-        if (rb.velocity.magnitude <= 0.05f) targ = transform.position + new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
-        disttotarg = Vector3.Distance(transform.position, targ);
-        if (disttotarg <= mindist) targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+        if (seenPlayer == true)
+        {
+            if (navigation.velocity.magnitude > speed * 1.5f) navigation.velocity = navigation.velocity.normalized * speed * 1.5f;
+            if (navigation.velocity.magnitude <= 0.05f) targ = transform.position + new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+            disttotarg = Vector3.Distance(transform.position, targ);
+            if (disttotarg <= mindist) targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+        }
+        else
+        {
+
+        }
     }
     private void FixedUpdate()
     {
         rb.AddForce((targ - transform.position).normalized * speed);
+
+        if (Physics.Raycast(transform.position, (player.transform.position - transform.position), out rayHit, sightLine * 1.5f))
+        {
+            if (rayHit.collider.transform.tag == "Player")
+            {
+                Debug.Log("I See You");
+                seenPlayer = true;
+
+            }
+            else if (rayHit.collider.transform.tag == "LevelPart")
+            {
+                Debug.Log("That's a wall");
+            }
+            else
+            {
+            }
+        }
     }
     public void TakeDamage(float d)
     {
