ddb74c7fd233064ea4a9c803684ba315cb773134
Sebastian <sebander01@gmail.com>
2023-03-07 02:29:11 -0500

    EnemyChanges

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -17,22 +17,56 @@ public class Enemy : MonoBehaviour
 
     private Rigidbody rb;
 
+    private bool seenPlayer = false;
+
+    private RaycastHit rayHit;
+    private Vector3 ray;
+
+    public float sightLine;
+
+    public Player player;
+
     private void Awake()
     {
+        player = FindObjectOfType<Player>();
         rb = GetComponent<Rigidbody>();
         currentHp = maxHp;
         targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
     }
     private void Update()
     {
-        if (rb.velocity.magnitude > speed * 1.5f) rb.velocity = rb.velocity.normalized * speed * 1.5f;
-        if (rb.velocity.magnitude <= 0.05f) targ = transform.position + new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
-        disttotarg = Vector3.Distance(transform.position, targ);
-        if (disttotarg <= mindist) targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+        if (seenPlayer == true)
+        {
+            if (rb.velocity.magnitude > speed * 1.5f) rb.velocity = rb.velocity.normalized * speed * 1.5f;
+            if (rb.velocity.magnitude <= 0.05f) targ = transform.position + new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+            disttotarg = Vector3.Distance(transform.position, targ);
+            if (disttotarg <= mindist) targ = new Vector3(Random.Range(0f, maxrange), 1f, Random.Range(0f, maxrange));
+        }
+        else
+        {
+
+        }
     }
     private void FixedUpdate()
     {
         rb.AddForce((targ - transform.position).normalized * speed);
+
+        if (Physics.Raycast(transform.position, (player.transform.position - transform.position), out rayHit, sightLine * 1.5f))
+        {
+            if (rayHit.collider.transform.tag == "Player")
+            {
+                Debug.Log("I See You");
+                seenPlayer = true;
+
+            }
+            else if (rayHit.collider.transform.tag == "LevelPart")
+            {
+                Debug.Log("That's a wall");
+            }
+            else
+            {
+            }
+        }
     }
     public void TakeDamage(float d)
     {
