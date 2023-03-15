5f936814c1f3e367d42695bf03945540d37b35be
Baldi7327 <94031987+Baldi7327@users.noreply.github.com>
2023-03-15 16:56:21 -0400

    MenuUpdate

--- Assets/Scripts/Game/Scenes/DeathMenu.cs
+++ Assets/Scripts/Game/Scenes/DeathMenu.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
 {
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void RespawnButton()
     {
+        Time.timeScale = 1f;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         SceneManager.LoadScene(1);
     }
     public void QuitButton()
     {
+        Debug.Log("Guit from death screen");
         Application.Quit();
     }
 }
